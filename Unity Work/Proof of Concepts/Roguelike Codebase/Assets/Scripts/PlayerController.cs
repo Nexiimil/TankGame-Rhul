a5c6e4c5335f0a246dd061334be892f328f78fd1
Sulaimaan Bajwa <zjac213@live.rhul.ac.uk>
2022-10-04 12:38:08 +0100

    Added: Testbox program where I attempt to move closer towards some approximation of my final project has been added to the proof of concepts folder. Hopefully this develops into a full fledged proof of concept, which will be uploaded as an executable additionally.

---
+++ Unity Work/Proof of Concepts/Roguelike Codebase/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour{
+[SerializeField]
+private Rigidbody2D rb;
+
+[SerializeField]
+private float[] position = {0,0};
+
+  void setRb(Rigidbody2D rb){this.rb = rb;}
+  Rigidbody2D getRb(){return this.rb;}
+
+  void setPosition(float[] position){this.position = position;}
+
+  void setPositionHorizontal(float horizontal){this.position[0] = horizontal;}
+  void setPositionVertical(float vertical){this.position[1] = vertical;}
+  float getPositionHorizontal(){return this.position[0];}
+  float getPositionVertical(){return this.position[1];}
+  
+  // Start is called before the first frame update
+  void Start(){
+      setRb(GetComponent<Rigidbody2D>());
+  }
+
+  // Update is called once per frame
+  void Update(){
+      
+  }
+}
