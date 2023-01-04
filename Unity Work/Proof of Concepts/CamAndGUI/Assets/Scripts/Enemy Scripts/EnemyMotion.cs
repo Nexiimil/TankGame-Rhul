f1d2fffeaa42fd91ae0ed8e90710a716f150c0e5
Sulaimaan Bajwa <zjac213@live.rhul.ac.uk>
2023-01-04 11:59:20 +0000

    Refactor: Commented all key lines. Addtionally, ensured getters and setters are exclusively used to access private fields.

--- Unity Work/Proof of Concepts/CamAndGUI/Assets/Scripts/Enemy Scripts/EnemyMotion.cs
+++ Unity Work/Proof of Concepts/CamAndGUI/Assets/Scripts/Enemy Scripts/EnemyMotion.cs
@@ -5,25 +5,21 @@ using UnityEngine.AI;
 
 public class EnemyMotion : MonoBehaviour
 {
-    [SerializeField]
-    private Transform targetLocation;
+    [SerializeField] private Transform targetLocation; //Ever changing location to head to
+    [SerializeField] private NavMeshAgent agent; //identifies this entity as an agent,
+                            //possessing some degree of automonomous ability to respond to a scenario
+    void setTargetLocation(Transform targetLocation){this.targetLocation = targetLocation;} //sets the location to head to
+    Transform getTargetLocation(){return this.targetLocation;} //fetches the location to head to
 
-    [SerializeField]
-    private NavMeshAgent agent;
-
-    void setTargetLocation(Transform targetLocation){this.targetLocation = targetLocation;}
-    Transform getTargetLocation(){return this.targetLocation;}
-
-    void setAgent(NavMeshAgent agent){this.agent = agent;}
-    NavMeshAgent getAgent(){return this.agent;}
+    void setAgent(NavMeshAgent agent){this.agent = agent;} //sets the agent after some adjustment perhaps
+    NavMeshAgent getAgent(){return this.agent;} //fetches the stored agent
 
     // Start is called before the first frame update
     void Start(){
-        agent = GetComponent<NavMeshAgent>();
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
         setAgent(agent);
-
         setTargetLocation(GameObject.FindGameObjectWithTag("Player").transform);
     }
     // Update is called once per frame
@@ -34,7 +30,5 @@ public class EnemyMotion : MonoBehaviour
             setTargetLocation(player.transform);
             agent.SetDestination(getTargetLocation().position);
         }
-        
-
     }
 }
--- Unity Work/Proof of Concepts/CamAndGUI/Assets/Scripts/Enemy Scripts/EnemyShooter.cs
+++ Unity Work/Proof of Concepts/CamAndGUI/Assets/Scripts/Enemy Scripts/EnemyShooter.cs
@@ -6,47 +6,28 @@ using UnityEngine;
 
 public class EnemyShooter : MonoBehaviour
 {
-    [SerializeField]
-    private Rigidbody2D rb;
-    
-    [SerializeField]
-    private Transform targetLocation;
+    [SerializeField] private Transform targetLocation; //Ever changing location to head to
+    [SerializeField] private float rotationSpeed = 30; //the speed at which rotation is allowed
 
-    [SerializeField]
-    private float rotation = 0;
+    void setTargetLocation(Transform targetLocation){this.targetLocation = targetLocation;} //sets the location to head to
+    Transform getTargetLocation(){return this.targetLocation;} //fetches the location to head to
 
-    [SerializeField]
-    private float rotationSpeed = 30;
-
-    void setRb(Rigidbody2D rb){this.rb = rb;}
-    Rigidbody2D getRb(){return this.rb;}
-
-    void setTargetLocation(Transform targetLocation){this.targetLocation = targetLocation;}
-    Transform getTargetLocation(){return this.targetLocation;}
-    void setRotationSpeed(float rotationSpeed){this.rotationSpeed = rotationSpeed;}
-    float getRotationSpeed(){return this.rotationSpeed;}
-    void setRotation(float rotation){this.rotation = rotation;}
-    float getRotation(){return this.rotation;}
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        setRb(GetComponent<Rigidbody2D>());
-    }
+    void setRotationSpeed(float rotationSpeed){this.rotationSpeed = rotationSpeed;} //sets the speed at which rotation is allowed
+    float getRotationSpeed(){return this.rotationSpeed;} //fetches the speed at which rotation is allowed
 
     // Update is called once per frame
-    void FixedUpdate(){
-        fetchPlayer();
+    void Update(){
+        fetchPlayer(); //ensures the players location is tracked
     }
     void fetchPlayer(){
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-        if (player != null){
-            setTargetLocation(player.transform);
+        GameObject player = GameObject.FindGameObjectWithTag("Player"); //enables check if a player is in play
+        if (player != null){ //player presence check
+            setTargetLocation(player.transform); //sets the location to head to as the players current location
 
-            Vector3 position = getTargetLocation().position - transform.position;
-            Quaternion rotation = Quaternion.LookRotation(Vector3.forward, position);
-            rotation *= Quaternion.Euler(0, 0, 90);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotationSpeed);
-        }
+            Vector3 position = getTargetLocation().position - transform.position; //creates a vector with the distance and direction toward the player
+            Quaternion rotation = Quaternion.LookRotation(Vector3.forward, position); //the vector acts as the real axis for a quaternion
+            rotation *= Quaternion.Euler(0, 0, 90); //correction for the placement of the cannon to face the player
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, getRotationSpeed());
+        }       //applies the quaternion rotation to the enemy, to face the payer
     }
 }
